14b3bdf7e6b2c9ccc766068001a5ce4fb498b7ae
Zorkrath <88572595+Zorkrath@users.noreply.github.com>
2021-12-09 20:24:04 -0700

    Add files via upload

---
+++ Login.py
@@ -0,0 +1,20 @@
+def login():
+
+
+    username = str("Enzo");
+
+    if username != str(input("Whats is your user name? : ")):
+        print("You did it wrong");
+    else:
+        print("")
+
+    password = int(3011);
+
+    if password != int(input("What's your password?: ")):
+        print("Get out of this account")
+    else:
+        print("Welcome")
+    
+
+    
+login()
